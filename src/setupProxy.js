3466e2c6cf1fb292e3715090994930d18f004be5
nataliep769 <nataliep769@gmail.com>
2021-03-08 12:22:14 -0600

    Changed proxy target to correct port

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,14 @@
-const createProxyMiddleware = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:3000',
+            target: 'http://localhost:3001',
             changeOrigin: true,
+            headers: {
+                "Connection": "keep-alive"
+            },
         })
     );
 };
